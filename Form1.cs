d6ee623c7e2459126baf17da47c4e9a0eee5d7be
Polina <Polina@WIN-5K2ODM26VIK>
2024-02-21 20:40:45 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Windows.Forms;
+
+namespace Gruz_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string connectionString = "Server=localhost;Port=5432;Database=Gruz;User ID=postgres;Password=2264";
+            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+
+            DataTable table = new DataTable();
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string sqlQuery = "SELECT * FROM \"Заказ\"";
+
+                using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(sqlQuery, conn))
+                {
+                    adapter.Fill(table);
+                }
+            }
+
+            dataGridView1.DataSource = table;
+        }
+    }
+}
